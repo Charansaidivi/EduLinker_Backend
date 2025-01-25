4cef8cb647f1778e2e5955ef9d561b6425b7495e
CharanSmart2003 <pappucharannarayanaswamy@gmail.com>
2025-01-25 17:04:27 +0530

    first time pushing

---
+++ model/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },// Topics the user is interested in (for learners) or teaches (for teachers)
+  profileImage: { type: String, default: null }, // URL of the profile image
+  ratings: { type: Number, default: 0 }, // Teacher's rating; learners won't need this
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
